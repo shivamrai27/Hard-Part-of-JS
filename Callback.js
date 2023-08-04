03d0f92b6d45afc12b85459c1866b1220062c679
Shivam Rai <200shivamrai@gmail.com>
2023-08-04 23:00:39 +0530

    Callback

--- Callback.js
+++ Callback.js
@@ -1,1 +1,14 @@
-console.log("Hello Callback");
+// Exercise 1
+// function addTwo(n) {
+//     return n + 2;
+// }
+// console.log(addTwo(2));
+
+// Exercise 2
+// function addS(word) {
+//     return word + 's';
+// }
+// console.log(addS('pen'));
+
+
+
